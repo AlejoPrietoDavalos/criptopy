dde356cabdaf6efb25df950b927f92406184dd05
Alejo Prieto <alejo.prieto@extendeal.com>
2024-02-04 04:24:19 -0300

    Utilidades de manejo de tiempo

--- cripto_trading/time.py
+++ cripto_trading/time.py
@@ -3,6 +3,21 @@
 """
 from datetime import datetime, timezone
 
-def time_ms_to_datetime_utc(timestamp_ms: int) -> datetime:
+
+def timestamp2datetime_utc(timestamp_ms: int) -> datetime:
     """ Recibe un timestamp en `ms`, retorna el datetime en UTC."""
     return datetime.fromtimestamp(timestamp_ms / 1000, tz=timezone.utc)
+
+
+def datetime2timestamp_utc(date: datetime) -> bool:
+    if not is_datetime_utc(date):
+        raise ValueError("El datetime debe estar en UTC.")
+    return int(date.timestamp() * 1000)   # Se convierte a ms.
+
+
+def is_datetime_utc(date: datetime) -> bool:
+    """ Retorna True si el datetime está en UTC."""
+    if date.tzinfo is not None:
+        if date.tzinfo.utcoffset(date) == timezone.utc.utcoffset(date):
+            return True
+    return False
