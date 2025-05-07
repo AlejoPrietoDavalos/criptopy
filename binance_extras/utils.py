1c56daaf34a62e147ddec01f002f8ad134bf2770
AlejoPrietoDavalos <alejoprietodavalos@gmail.com>
2025-05-07 10:01:16 -0300

    type

--- binance_extras/utils.py
+++ binance_extras/utils.py
@@ -1,15 +1,15 @@
 from typing import Tuple, NewType
-from dotenv import load_dotenv
 import os
-load_dotenv()
 
+from dotenv import load_dotenv
 from binance.um_futures import UMFutures
 
+load_dotenv()
 
-BinanceApiKey = NewType("ApiKey", str)
-BinanceSecretKey = NewType("SecretKey", str)
+type T_BinanceApiKey = str
+type T_BinanceSecretKey = str
 
-def get_credentials() -> Tuple[BinanceApiKey, BinanceSecretKey]:
+def get_credentials() -> Tuple[T_BinanceApiKey, T_BinanceSecretKey]:
     BINANCE_API_KEY = os.getenv("BINANCE_API_KEY")
     BINANCE_SECRET_KEY = os.getenv("BINANCE_SECRET_KEY")
     return BINANCE_API_KEY, BINANCE_SECRET_KEY
